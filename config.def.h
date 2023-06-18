33d34d1b260c71779cb213b1d316dc7003b94788
Novores <novalhidayat@tuta.io>
2023-06-19 01:55:25 +0700

    update config

--- config.def.h
+++ config.def.h
@@ -1,7 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -11,29 +14,37 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "monospace:size=10";
-static char dmenufont[]       = "monospace:size=10";
+static char font[]            = "JetBrains Mono:style=Medium:size=9.5";
 static const char *fonts[]          = { font };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char dmenufont[]       = "JetBrains Mono:size=10";
+static char normbgcolor[]           = "#1d2021";
+static char normfgcolor[]           = "#ebdbb2";
+static char normbordercolor[]       = "#3c3836";
+static char selbgcolor[]            = "#1d2021";
+static char selfgcolor[]            = "#d8a657";
+static char selbordercolor[]        = "#d8a657";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
-       [SchemeTitle]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       [SchemeTitle]  = { selfgcolor, selbgcolor, selbordercolor  },
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+  "feh", "--bg-fill", "/home/novores/Pictures/wallpapers/background.png", NULL,
+  "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1", NULL,
+  "xbanish", NULL,
+  "usermount", NULL,
+  "xrdb", "/home/novores/.Xresources", NULL,
+  "xbacklight", "-set", "50", NULL,
+  "sh", "-c", "xset s 240 60; xset dpms 301;xss-lock -n ~/.local/bin/dim-screen.sh -- betterlockscreen --lock &", NULL,
+  "sh", "-c", "pkill bar.sh; /home/novores/.local/bin/dwmstatus", NULL,
+  "xcompmgr", "-c", "-C", "-t-5", "-l-5", "-r4.2", "-o.55", NULL,
 	NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
@@ -46,8 +57,18 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+  {"qutebrowser",             NULL,       NULL,       1 << 1,       0,           -1},
+  {"Firefox",                 NULL,       NULL,       1 << 1,       0,           -1},
+  {"Soffice",             NULL,       NULL,       1 << 2,       0,           -1},
+  {"Inkscape",                NULL,       NULL,       1 << 3,       0,           -1},
+  {"Gimp",                    NULL,       NULL,       1 << 3,       0,           -1},
+  {"SimpleScreenRecorder",    NULL,       NULL,       0,            1,           -1},
+  {"mpv",                     NULL,       NULL,       0,            1,           -1},
+  {"feh",                     NULL,       NULL,       0,            1,           -1},
+  {"Lxappearance",            NULL,       NULL,       0,            1,           -1},
+  {"qt5ct",                   NULL,       NULL,       0,            1,           -1},
+  {"aft-linux-qt",            NULL,       NULL,       0,            1,           -1},
+  {"KeePassXC",               NULL,       NULL,       0,            1,           -1},
 };
 
 /* layout(s) */
@@ -66,20 +87,29 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+// library keyxf86
+#include <X11/XF86keysym.h>
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static char dmenumon[2] = "1"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-p", "Apps", "-m", dmenumon, "-fn", dmenufont, "-nb", selfgcolor, "-nf", normbgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-hp", "firefox,inkscape,qutebrowser,localc,lobase,lowriter,android-file-transfer,gimp,nextcloud,alacritty,steam,obs,shotcut,pavucontrol,lxappearance, poweroff, reboot, sleep", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *volup[] = {"pamixer", "-i", "2", NULL};
+static const char *voldown[] = {"pamixer", "-d", "2", NULL};
+static const char *mute[] = {"pamixer", "-t", NULL};
+static const char *mpcpl[] = {"mpc", "toggle", NULL};
+static const char *mpcnext[] = {"mpc", "next", NULL};
+static const char *mpcprev[] = {"mpc", "prev", NULL};
+static const char *brup[] = {"xbacklight", "-inc", "2", NULL};
+static const char *brdown[] = {"xbacklight", "-dec", "2", NULL};
 
 /*
  * Xresources preferences to load at startup
@@ -94,18 +124,18 @@ ResourcePref resources[] = {
 		{ "selbordercolor",     STRING,  &selbordercolor },
 		{ "selfgcolor",         STRING,  &selfgcolor },
 		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
+		{ "snap",               INTEGER, &snap },
 		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
+		{ "topbar",             INTEGER, &topbar },
 		{ "nmaster",          	INTEGER, &nmaster },
 		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+		{ "mfact",              FLOAT,   &mfact },
 };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -116,9 +146,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -160,7 +190,31 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  { MODKEY|ControlMask,           XK_q,        quit,         {0} }, 
+
+  //command using xf86 keys
+  {0,                             XF86XK_AudioMute,           spawn,  {.v = mute}},
+  {0,                             XF86XK_AudioRaiseVolume,    spawn,  {.v = volup}},
+  {0,                             XF86XK_AudioLowerVolume,    spawn,  {.v = voldown}},
+  {0,                             XF86XK_AudioPlay,           spawn,  {.v = mpcpl}},
+  {0,                             XF86XK_AudioNext,           spawn,  {.v = mpcnext}},
+  {0,                             XF86XK_AudioPrev,           spawn,  {.v = mpcprev}},
+  {0,                             XF86XK_MonBrightnessUp,     spawn,  {.v = brup}},
+  {0,                             XF86XK_MonBrightnessDown,   spawn,  {.v = brdown}},
+
+  // Misc
+  {ALTKEY,                        XK_m,       spawn,        SHCMD("mpd && dunstify 'mpd' 'online'")},
+  {ALTKEY|ShiftMask,              XK_m,       spawn,        SHCMD("pkill mpd && dunstify 'mpd' 'offline'")},
+  {ALTKEY,                        XK_Left,    spawn,        {.v = mpcprev}},
+  {ALTKEY,                        XK_Right,   spawn,        {.v = mpcnext}},
+  {ALTKEY,                        XK_Up,      spawn,        {.v = mpcpl}},
+  {ALTKEY,                        XK_Down,    spawn,        SHCMD("mpc stop")},
+  {ALTKEY,                        XK_p,       spawn,        SHCMD("rbwmenu")},
+  {ALTKEY,                        XK_t,       spawn,        SHCMD("transmenu")},
+  {ALTKEY,                        XK_i,       spawn,        {.v = volup}},
+  {ALTKEY,                        XK_d,       spawn,        {.v = voldown}},
+  {ALTKEY|ControlMask,            XK_i,       spawn,        {.v = brup}},
+  {ALTKEY|ControlMask,            XK_d,       spawn,        {.v = brdown}},
 };
 
 /* button definitions */
@@ -179,4 +233,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
